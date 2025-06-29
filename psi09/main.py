ddc2f638174943090a2013fa04d192b12affb0c9
sudoboneman <boneman7908@gmail.com>
2025-06-30 04:06:23 +0530

    Fix Flask port binding for Render

--- psi09/main.py
+++ psi09/main.py
@@ -94,4 +94,5 @@ def psi09():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
