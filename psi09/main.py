37ca3dc076a1db3b644ea46c376c14c56dde11ce
sudoboneman <boneman7908@gmail.com>
2025-06-30 04:30:28 +0530

    reverted port change

--- psi09/main.py
+++ psi09/main.py
@@ -100,5 +100,4 @@ def psi09():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 locally
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
